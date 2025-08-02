6d939ab727d4976db7a271090c0f3d354e2d6575
RAAGHAVENDRAA <53081166+RAAGHAVENDRAA@users.noreply.github.com>
2025-08-02 22:38:55 +0530

    Update1 entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Generate Mosquitto config from template
-envsubst < /app/etc/mosquitto/mosquitto.template.conf > /etc/mosquitto/mosquitto.conf
+envsubst < /app/etc/mosquitto/mosquitto.conf > /etc/mosquitto/mosquitto.conf
 
 # Start Mosquitto in background
 /usr/sbin/mosquitto -c /etc/mosquitto/mosquitto.conf &
