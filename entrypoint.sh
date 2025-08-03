c76ddb7db982b8b3d15e93521130990c90a99198
RAAGHAVENDRAA <53081166+RAAGHAVENDRAA@users.noreply.github.com>
2025-08-03 08:24:37 +0530

    Updatenn1 entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 
 # Generate Mosquitto config from template
-envsubst < /app/etc/mosquitto/mosquitto.conf > /etc/mosquitto/mosquitto.conf
+#envsubst < /app/etc/mosquitto/mosquitto.conf > /etc/mosquitto/mosquitto.conf
 
 # Start Mosquitto in background
-/usr/sbin/mosquitto -c /etc/mosquitto/mosquitto.conf &
+#/usr/sbin/mosquitto -c /etc/mosquitto/mosquitto.conf &
 
 # Start the WebSocket proxy
+#node /app/server.js
+
+
+#!/bin/sh
+
+envsubst < /app/etc/mosquitto/mosquitto.conf > /etc/mosquitto/mosquitto.conf
+/usr/sbin/mosquitto -c /etc/mosquitto/mosquitto.conf &
+
+sleep 2  # Give Mosquitto time to start
+
 node /app/server.js
